b4685e75b989a489cec457cd70b40a00839f711f
salim963 <salimdk97@gmail.com>
2022-11-06 15:11:46 +0100

    dockerfile & shopping card con

---
+++ webshop-backend/Controllers/ShoppingcartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webshop_backend.Model.Products;
+using webshop_backend.Repositories;
+
+namespace webshop_backend.Controllers
+{
+    public class ShoppingcartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
--- webshop-backend/Program.cs
+++ webshop-backend/Program.cs
@@ -1,5 +1,6 @@
 using webshop_backend.Repositories;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
