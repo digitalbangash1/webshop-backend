e1037006feea527aa8c16a8c728332af6b8697f3
salim963 <70900247+salim963@users.noreply.github.com>
2022-11-22 12:27:31 +0100

    .

--- webshop-backend/Program.cs
+++ webshop-backend/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
 
+
 //DI
 builder.Services.AddScoped<IDbConnectionService, DbConnectionService>();
 builder.Services.AddScoped<IPersonRespository, PersonRespository>();
@@ -38,3 +39,20 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            // Add the following line:
+            webBuilder.UseSentry(o =>
+            {
+                o.Dsn = "https://3c2d9f76bf0048108e065a109c10a16e@o4504162605268992.ingest.sentry.io/4504162746499072";
+                // When configuring for the first time, to see what the SDK is doing:
+                o.Debug = true;
+                // Set TracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+                // We recommend adjusting this value in production.
+                o.TracesSampleRate = 1.0;
+            });
+        });
