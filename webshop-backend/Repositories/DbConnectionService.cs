d96116052ae0ed2b19dceb457640df41d20d939b
Muhammad Bangash <s092512@student.dtu.dk>
2022-11-30 18:52:48 +0100

    dc connection changed

--- webshop-backend/Repositories/DbConnectionService.cs
+++ webshop-backend/Repositories/DbConnectionService.cs
@@ -7,13 +7,13 @@ namespace webshop_backend.Repositories
     public class DbConnectionService : IDbConnectionService
 
     {
+        private static string connectionString = "server=mysql-database-db.admin.samat.diplomportal.dk;port=3306;database=webshop;user=root;password=salim123";
+        //
+        //"server=localhost;port=3306;database=test1;user=root;password=123456"
+
+
         //TODO this should not be in code but in appsettings.json
-        // private static string connectionString = "server=localhost;port=3306;database=webshop;user=root;password=salim123";
-        private static string connectionString = "server=localhost;port=3306;database=test1;user=root;password=123456";
-        //"server=mysql-database-db.admin.samat.diplomportal.dk;port=3306;database=webshop;user=root;password=salim123";
-
-
-
+        // // private static string connectionString = "server=localhost;port=3306;database=webshop;user=root;password=salim123";
         //"server=mysql-database-db.admin.samat.diplomportal.dk;port=3306;database=webshop;user=root;password=salim123";
         //"server=localhost;port=3306;database=webshop;user=root;password=Wajiha3330";
         //
